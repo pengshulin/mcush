c4f6feb872f42b3dde2a187448d8c37c8c36a83b
Peng Shulin <trees_peng@163.com>
2017-05-15 08:45:08 +0800

    edit

--- appDemoLua/main.c
+++ appDemoLua/main.c
@@ -1,20 +1,25 @@
 #include "mcush.h"
 
+#define USE_LUA
+
 
 extern int lua_main (int argc, char **argv);
 
+#ifdef USE_LUA
 static shell_cmd_t cmd_tab[] = {
 {   "l", "lua", lua_main, 
     "run lua interpreter",
     "lua <fname>"  },
 {   0,  0,  0,  0  } };
-
+#endif
 
 
 int main(void)
 { 
     mcush_init();
+#ifdef USE_LUA
     shell_add_cmd_table( cmd_tab );
+#endif
     mcush_start();
     while(1);
 }
