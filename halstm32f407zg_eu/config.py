6802a9509b21dd05f6112db042bed46191967105
Peng Shulin <trees_peng@163.com>
2018-02-13 15:10:41 +0800

    edit

--- halstm32f407zg_eu/config.py
+++ halstm32f407zg_eu/config.py
@@ -6,8 +6,8 @@ port = 'ARM_CM4F'
 env.appendDefineFlags( [ 'HSE_VALUE=8000000' ] )
 env.appendDefineFlags( [ 'HAL_RNG=1' ] )
 # NOTE:
-# FCFS from 0x081E0000 ~ 0x081FFFFF contains 128Kbytes, this wastes a lot
-env.appendDefineFlags( [ 'MCUSH_FCFS=1', 'FCFS_ADDR=0x081E0000', ] )
+# FCFS from 0x080E0000 ~ 0x080FFFFF contains 128Kbytes, this wastes a lot
+env.appendDefineFlags( [ 'MCUSH_FCFS=1', 'FCFS_ADDR=0x080E0000', ] )
 
 
 USE_VFS=False
