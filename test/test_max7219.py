59a54a896fe2083c03613ae4d3c7e61befdfc350
Peng Shulin <trees_peng@163.com>
2018-05-11 09:58:56 +0800

    edit

--- test/test_max7219.py
+++ test/test_max7219.py
@@ -25,6 +25,15 @@ ADDR_SHUTDOWN = 12
 ADDR_DISP_TEST = 15
 
 
+def B2I( bitmap, reverse ):
+    assert len(bitmap) == 8
+    ret = 0
+    for i in range(8):
+        if bitmap[i] == '*':
+            ret |= 1<<(7-i)
+    return (ret ^ 0xFF) if reverse else ret
+
+
 class MAX7219(Mcush.Mcush):
     def init(self):
         self.spi_init(width=16, delay=100)
@@ -47,19 +56,120 @@ class MAX7219(Mcush.Mcush):
         self.write( ADDR_D6, 0x55 )
         self.write( ADDR_D7, 0xAA )
         self.write( ADDR_DISP_TEST, 0 )
-        
+
+    def draw_bitmap( self, bitmap, reverse=False ):
+        assert len(bitmap) == 8
+        self.write( ADDR_D0, B2I(bitmap[0], reverse))
+        self.write( ADDR_D1, B2I(bitmap[1], reverse))
+        self.write( ADDR_D2, B2I(bitmap[2], reverse))
+        self.write( ADDR_D3, B2I(bitmap[3], reverse))
+        self.write( ADDR_D4, B2I(bitmap[4], reverse))
+        self.write( ADDR_D5, B2I(bitmap[5], reverse))
+        self.write( ADDR_D6, B2I(bitmap[6], reverse))
+        self.write( ADDR_D7, B2I(bitmap[7], reverse))
+ 
+
+EMPTY = [
+    '--------',
+    '--------',
+    '--------',
+    '--------',
+    '--------',
+    '--------',
+    '--------',
+    '--------', ]
+
+
+HEART = [
+    '-**--**-',
+    '*--**--*',
+    '*------*',
+    '*------*',
+    '-*----*-',
+    '-*----*-',
+    '--*--*--',
+    '---**---', ]
+
+HEART2 = [
+    '-**--**-',
+    '********',
+    '********',
+    '********',
+    '-******-',
+    '-******-',
+    '--****--',
+    '---**---', ]
+
+
+FAN1 = [
+    '----*---',
+    '----*---',
+    '----*---',
+    '*****---',
+    '---*****',
+    '---*----',
+    '---*----',
+    '---*----', ]
+
+FAN2 = [
+    '--------',
+    '-*----*-',
+    '--*--*--',
+    '---**---',
+    '---**---',
+    '--*--*--',
+    '-*----*-',
+    '--------', ]
+
+DIGIT_0 = [
+    '--------',
+    '--****--',
+    '-**--**-',
+    '-**--**-',
+    '-**--**-',
+    '-**--**-',
+    '-**--**-',
+    '--****--', ]
+
+DIGIT_1 = [
+    '--------',
+    '---**---',
+    '--***---',
+    '---**---',
+    '---**---',
+    '---**---',
+    '---**---',
+    '--****--', ]
+
+DIGIT_2 = [
+    '--------',
+    '--****--',
+    '-******-',
+    '-**--**-',
+    '----**--',
+    '---**---',
+    '-******-',
+    '-******-', ]
+
+
+BITMAPS = [FAN1, FAN2]
+BITMAPS = [DIGIT_0, DIGIT_1, DIGIT_2]
+BITMAPS = [HEART, HEART2]
+
+
+
+
  
 def main(argv=None):
     s = MAX7219()
     s.init()
     s.reset()
-    for i in range(16):
-        s.write( ADDR_INTENSITY, i )
-        time.sleep(0.2)
+    s.write( ADDR_INTENSITY, 2 )
     counter = 1
     while True:
-        s.write( ADDR_D0, counter & 0xFF )
-        time.sleep(0.1)
+        for img in BITMAPS:
+            s.draw_bitmap( img )
+            time.sleep(0.5)
         counter += 1
 
 
