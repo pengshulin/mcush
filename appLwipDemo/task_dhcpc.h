0a208832f8bd5b36ad7d6dc2660bc5fa888a17db
Peng Shulin <trees_peng@163.com>
2018-12-29 13:07:46 +0800

    edit

--- appLwipDemo/task_dhcpc.h
+++ appLwipDemo/task_dhcpc.h
@@ -16,7 +16,7 @@
 void net_state_change_hook(int connected);
 #endif
  
-void send_dhcpc_event(uint8_t event); 
+int send_dhcpc_event(uint8_t event); 
 
 void task_dhcpc_init(void);
 void logger_ip( const char *prompt, uint32_t address, int shell_mode );
