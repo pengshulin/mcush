4dee872c2a97e76eabff5a0a0efb4face797e54c
Peng Shulin <trees_peng@163.com>
2020-08-17 23:04:59 +0800

    word layout

--- appBlinkErrorNumber/task_blink.c
+++ appBlinkErrorNumber/task_blink.c
@@ -3,56 +3,57 @@
  * The errno is a multi-digital positive integer number, it's different from
  * newlib's reentrant errno, don't get confused.
  *
- * The number is blinked digital-by-digtal from left to right (up to 9 digits).
+ * The number blinks digital-by-digtal from left to right (up to 9 digits).
  * LED flashes quickly for 1~9 and slow for 0.
  * A longer delay time is necessary to help you sync to a new round.
+ *
  * eg.
  * errno 0 (power up default):
  *     the LED is set ON for 1s and OFF for 1s (blinks slowly as 2Hz)
- *       ____________________                      ____________________
- *  ____|                    |____________________|                    |_____
+ *      ____________________                      ____________________
+ * ____|                    |____________________|                    |_____
  *
  * errno 1
  *     the LED set ON for 0.2s and OFF for 1s
- *       ____                      ____                      ____            
- *  ____|    |____________________|    |____________________|    |___________
+ *      ____                      ____                      ____            
+ * ____|    |____________________|    |____________________|    |___________
  *
  * errno 3
  *     the LED set ON 3times and OFF for 1s
- *       ____      ____      ____                      ____      ____      __
- *  ____|    |____|    |____|    |____________________|    |____|    |____|  
+ *      ____      ____      ____                      ____      ____      __
+ * ____|    |____|    |____|    |____________________|    |____|    |____|  
  * 
  * errno 21
  *     short delay is inserted between 2 and 1
- *       ____      ____            ____                      ____      ____  
- *  ____|    |____|    |__________|    |____________________|    |____|    |_
+ *      ____      ____            ____                      ____      ____  
+ * ____|    |____|    |__________|    |____________________|    |____|    |_
  *
  * errno 102
- *       ____            ____________________            ____      ____            
- *  ____|    |__________|                    |__________|    |____|    |_____
+ *      ____            ____________________            ____      ____      
+ * ____|    |__________|                    |__________|    |____|    |_____
  *
  *
- * This task runs as an auxiliary service in a micro-kerenl RTOS environment,
+ * The task runs as an auxiliary service in a micro-kerenl RTOS environment,
  * it aims to help you debug your applications.
  * Especially when you're porting/debugging a new board, it may runs as the
  * first demo task.
  * Other tasks may call set(get)_errno when some events occur.
  * If a negative value is set, the task will stop blinking.
  * 
- * To use this service task, copy or symbolic link task_blink.c/h files to your
- * application and call task_blink_init() before you use it.
+ * To use this service task, copy or symbolic link task_blink.c/h files to
+ * your application and call task_blink_init() before you use it.
  *
  * An independent shell command 'error' (alias name 'e') can
  * modify the number in runtime, and stop the task from blinking.
  *
- * As this maybe the only running task, it will periodically call hal_wdg_clear to
- * feed the watchdog.
- * Task priority is set OS_PRIORITY_NORMAL (same as shell task), modify it if
- * necessary.
+ * As this may be the only running task, it will periodically call 
+ * hal_wdg_clear to feed the watchdog.
+ * Task priority is set OS_PRIORITY_NORMAL (same as shell task), modify
+ * it if * necessary.
  * 
  * The macro ERRNO_LED_NORMAL is set for errno zero, ERRNO_LED_ERROR for 
- * other non-zero number, normally they are both set to the same led (index 0).
- * You can change them to two different leds (use a RED led for warning/error
+ * other non-zero numbers, normally they are both set to the same #0 led.
+ * You can change them to different leds (use a RED led for warning/error
  * maybe a good ieda).
  *
  * MCUSH designed by Peng Shulin, all rights reserved. */
@@ -157,9 +158,10 @@ static void blink_digit( int digit, int led )
             os_task_delay(TICKS_NONZERO);
             hal_led_clr(led);
             os_task_delay(TICKS_NONZERO);
-            /* NOTE:  if the blink task is the only running task, in release mode,
-            cpu may reboot by the wdg (especially for long digits error code),
-            so clear the wdg on every blink */
+            /* NOTE:
+            if the blink task is the only running task, in release mode,
+            cpu may reboot by the wdg (especially for long digits error 
+            code), so clear the wdg on every blink */
             hal_wdg_clear();
         }
     }
