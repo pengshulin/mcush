436dab5138479d0926088f239dc98f316b174f0b
Peng Shulin <trees_peng@163.com>
2021-08-17 15:25:43 +0800

    update pypi twine update to python3

--- test/pypi_update.sh
+++ test/pypi_update.sh
@@ -5,9 +5,9 @@
 # Peng Shulin <trees_peng@163.com>
 #
 
-python setup.py sdist
+python3 setup.py sdist
 
-VER=`python -c 'import mcush; print mcush.__version__'`
+VER=`python3 -c 'import mcush; print(mcush.__version__)'`
 TAR=dist/mcush-$VER.tar.gz
 
 test -f $TAR && twine upload $TAR
